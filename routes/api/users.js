9cb752e1037a21144e8c6d5ab636928902e425f4
Josh Choi <joshuachoi0508@gmail.com>
2018-12-28 19:44:19 -0800

    Finish project

---
+++ routes/api/users.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post("/register", (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ handle: req.body.handle }).then(user => {
+        if (user) {
+            errors.handle = "User already exists";
+            return res.status(400).json(errors);
+        } else {
+            const newUser = new User({
+                handle: req.body.handle,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => {
+                            const payload = { id: user.id, handle: user.handle };
+
+                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            });
+                        })
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success' });
+                    } else {
+                        errors.password = 'Incorrect password'
+                        return res.status(400).json(errors);
+                    }
+                })
+        })
+})
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
+})
+
+module.exports = router;

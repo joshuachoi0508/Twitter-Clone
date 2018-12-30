c55786910f0fd86bc034486b0e8c5641d98ceb82
Josh Choi <joshuachoi0508@gmail.com>
2018-12-29 19:38:52 -0800

    Add heroku scripts/paths

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 mongoose
 .connect(db, { useNewUrlParser: true })
--- routes/api/users.js
+++ routes/api/users.js
@@ -71,10 +71,21 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: 'Success' });
+                        const payload = { id: user.id, name: user.name };
+
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            // Tell the key to expire in one hour
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
                     } else {
-                        errors.password = 'Incorrect password'
-                        return res.status(400).json(errors);
+                        return res.status(400).json({ password: 'Incorrect password' });
                     }
                 })
         })
